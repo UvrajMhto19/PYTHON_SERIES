9633394acccab5bf2474dc77d78bbb6ac6a36d6c
yuvrajkumar <yuvrajkumarkumar283@gmail.com>
2026-07-18 19:35:13 +0530

    Add files via upload

---
+++ Variable&dataType/print_statement.py
@@ -0,0 +1,13 @@
+# name = "Ankit"
+# age = 20
+# age += 5
+# gender = 'Male'
+
+# print(f"Your name is {name}, age is {age + 5}, and your gender is {gender}")
+# print(age, name, gender)
+
+
+age = 20
+Age = 25
+AGE = 30
+print(age, Age, AGE)
