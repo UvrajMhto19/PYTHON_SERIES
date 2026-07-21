a9f6e0fdd0e2366e5c21d49be636204f90f849b5
yuvrajkumar <yuvrajkumarkumar283@gmail.com>
2026-07-21 20:15:28 +0530

    Add arithmetic operations and escape sequences

---
+++ Escape sequence and variable/arithmetic_operater.py
@@ -0,0 +1,18 @@
+a = 12
+b = 15
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+#float division
+print(281 // 31)
+
+#Exponent
+print(9 ** 0.5)
+
+print(59 ** 0.51)
+
+#Modulus
+print(67 % 10)
