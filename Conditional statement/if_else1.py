0f863b2d1ca0777e87da5c370e728cf4d8c79194
yuvrajkumar <yuvrajkumarkumar283@gmail.com>
2026-07-21 20:21:24 +0530

    Create if_else1.py

---
+++ Conditional statement/if_else1.py
@@ -0,0 +1,8 @@
+physics = int(input("Enter your physics marks = "))
+chemistry = int(input("Enter your chemistry marks = "))
+
+if physics > 35 and chemistry > 35:
+    print("Pass")
+
+else:
+    print("Fail")
