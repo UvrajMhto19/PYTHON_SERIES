b5fce759dfeb0b0094e4f8c55374eaae2d6f1458
yuvrajkumar <yuvrajkumarkumar283@gmail.com>
2026-07-21 20:22:29 +0530

    Add files via upload

---
+++ Conditional statement/if_else3_nested.py
@@ -0,0 +1,11 @@
+age = 50
+certificate = True
+
+if age >= 18:
+    if certificate == True:
+        print("you are hired")
+    else:
+        print("you dont hired due to certificate")
+
+else:
+    print("you can'not hired, becouse of your age is less than 18")
