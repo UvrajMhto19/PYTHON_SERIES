9b59e9dcb5d28a5d27bfed8742888d563b57e74c
yuvrajkumar <yuvrajkumarkumar283@gmail.com>
2026-07-21 20:18:12 +0530

    Add files via upload

---
+++ Escape sequence and variable/logical_operater.py
@@ -0,0 +1,9 @@
+Math = 89
+Pysics = 90
+Chemistry = 75
+
+print(Math > 35 and Pysics > 35 and Chemistry > 35)
+
+print(Math > 35 or Pysics > 35 or Chemistry > 35)
+
+print(not(Math > 35 and Pysics > 35 and Chemistry > 35))
