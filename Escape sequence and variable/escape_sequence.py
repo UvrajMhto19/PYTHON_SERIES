9768efd471794204e3afc063181c3a8eedff01c4
yuvrajkumar <yuvrajkumarkumar283@gmail.com>
2026-07-21 20:17:50 +0530

    Add files via upload

---
+++ Escape sequence and variable/escape_sequence.py
@@ -0,0 +1,18 @@
+print("He\nllo\nWor\nld")
+
+print("He\tllo\tWorld")
+
+print("He\\lovel\\someone")
+
+print("He\"love\"someone")
+
+print("He\'love\'once")
+
+"""
+\n = new line
+\t = tab space
+\\ = backslash
+\' = single quote
+\" = double quote
+
+"""
